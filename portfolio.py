960a12ff2532f2e438bfe155c239cd76c2d82627
Markus Meissner <markus@evigo.net>
2014-11-30 22:12:39 +0100

    added config.py.tmpl - just copy to config.py and change the configuration values accordingly... cmdline-args parsing with argparser in portfolio.py ... load config, cmdline may overwrite it, smarter cmd-line-arg-handling, in portfolio.py replaced '%' with .format()-syntax, cleanups, minor typos

--- portfolio.py
+++ portfolio.py
@@ -1,82 +1,106 @@
-import sys
+import sys, os
+import argparse
 
 from classes import Havelock
 from classes import Bitcoin
 
-
-if __name__ == "__main__":
-    bitcoin = Bitcoin("xxx")
-    if len(sys.argv) == 3:
-        bitcoin.loadTransactionFile(sys.argv[2])
-    else:
-        print "tip: run this script with python2 %s <havelock-csv-file> <bitcoin-csv-file> to get an initial state" % sys.argv[0]
-
-    havelock = Havelock("xxx")
-    if len(sys.argv) == 2:
-        havelock.loadTransactionFile(sys.argv[1])
-    else:
-        print "tip: run this script with python2 %s <havelock-csv-file> to get an initial state" % sys.argv[0]
+from config import Config
 
 
-    while 1:
-        # update transactions
-        havelock.fetchTransactions()
-        # get current prices
-        havelock.fetchPortfolio()
-        havelock.printDetails(full=False)
+cmdline_parse = cp = \
+    argparse.ArgumentParser(
+            description="BTC.de and Havelock portfolio manager")
 
-        havelockBalance = havelock.getBalance()
+cp.add_argument("-B", "--btcde-csv-file", type=str, 
+        default=Config.btc_de_history,
+        help="Bitcoin.de transaction log / history")
+cp.add_argument("--btcde-api-key", type=str, 
+        default=Config.btc_de_api_key,
+        help="The API key to be used for bitcoin.de")
 
-        havelock.store()
-        
-        print 
+cp.add_argument("-H", "--havelock-csv-file", type=str, 
+        default=Config.hl_history,
+        help="Havelock transaction log / history")
+cp.add_argument("--havelock-api-key", type=str, 
+        default=Config.hl_api_key,
+        help="The API key to be used for Havelock")
 
-        bitcoin.fetchData()
-        bitcoin.printDetails(full=False)
-        
-        print 
+args = cp.parse_args()
 
-        bitcoinBalance = bitcoin.getBalance()
+bitcoin = Bitcoin(args.btcde_api_key)
+havelock = Havelock(args.havelock_api_key)
 
-        print "Summery:"
-        print "------------------------------"
-        sumBtc = bitcoinBalance + havelockBalance
-        sumEur = bitcoin.exchange(sumBtc)
-        print "Total sum: %f BTC (%0.2f EUR)" % (sumBtc, sumEur)
-        invest = bitcoin.getInvest()
-        print "Total sum of invest: %0.2f EUR" % invest
-        print "in sum your profit is: %0.2f EUR" % (sumEur + invest)
-        break
+fn = args.btcde_csv_file
+if os.path.exists(fn) and os.path.isfile(fn):
+    bitcoin.loadTransactionFile(fn)
+else:
+    print "[-] no bitcoin.de transaction history found..."
 
-        time.sleep(10)
+fn = args.havelock_csv_file
+if os.path.exists(fn) and os.path.isfile(fn):
+    havelock.loadTransactionFile(fn)
+else:
+    print "[-] no havelock transaction history found..."
+    
+while 1:
+    # update transactions
+    havelock.fetchTransactions()
+    # get current prices
+    havelock.fetchPortfolio()
+    havelock.printDetails(full=False)
+
+    havelockBalance = havelock.getBalance()
+
+    havelock.store()
+    
+    print 
+
+    bitcoin.fetchData()
+    bitcoin.printDetails(full=False)
+    
+    print 
+
+    bitcoinBalance = bitcoin.getBalance()
+
+    print "Summary:"
+    print "------------------------------"
+    sumBtc = bitcoinBalance + havelockBalance
+    sumEur = bitcoin.exchange(sumBtc)
+    print "Total sum: {:.8f} BTC ({:.2f} EUR)".format(sumBtc, sumEur)
+    invest = bitcoin.getInvest()
+    print "Total sum of invest: {:.2f} EUR".format(invest)
+    print "in sum your profit is: {:.2f} EUR".format(sumEur + invest)
+    break
+
+    time.sleep(10)
 
 
-    """
-    # debug win / loss shit
-    data = havelock.getActivity()
-    cnt = 0
-    data2 = []
-    for (ts, balance) in data:
-        data2.append((cnt, balance))
-        cnt +=1
+"""
+# debug win / loss shit
+data = havelock.getActivity()
+cnt = 0
+data2 = []
+for (ts, balance) in data:
+    data2.append((cnt, balance))
+    cnt +=1
 
-    data3 = havelock.getData("B.MINE")
-    data4 = havelock.getData("AMHASH1")
-    data5 = havelock.getData("SCRYPT")
-    data6 = havelock.getData("PETA")
+data3 = havelock.getData("B.MINE")
+data4 = havelock.getData("AMHASH1")
+data5 = havelock.getData("SCRYPT")
+data6 = havelock.getData("PETA")
 
-    import numpy as np
-    import matplotlib.pyplot as plot
+import numpy as np
+import matplotlib.pyplot as plot
 
-    fig = plot.figure()
-    ax = fig.add_subplot(111)
-    (xes, yes) = zip(*data3)
-    ax.plot(xes, yes, 'b-')
-    (xes, yes) = zip(*data4)
-    ax.plot(xes, yes, 'r-')
-    (xes, yes) = zip(*data5)
-    ax.plot(xes, yes, 'k-')
-    (xes, yes) = zip(*data6)
-    ax.plot(xes, yes, 'y-')
-    plot.show()"""
+fig = plot.figure()
+ax = fig.add_subplot(111)
+(xes, yes) = zip(*data3)
+ax.plot(xes, yes, 'b-')
+(xes, yes) = zip(*data4)
+ax.plot(xes, yes, 'r-')
+(xes, yes) = zip(*data5)
+ax.plot(xes, yes, 'k-')
+(xes, yes) = zip(*data6)
+ax.plot(xes, yes, 'y-')
+plot.show()"""
 
