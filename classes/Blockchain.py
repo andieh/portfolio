25d89248931f276cb90dbc77213c3804b19ec57b
andieh <mail@andieh.de>
2014-12-12 01:51:56 +0100

    try to calculate difficulty

--- classes/Blockchain.py
+++ classes/Blockchain.py
@@ -47,7 +47,43 @@ class Blockchain:
 if __name__ == "__main__":
     b = Blockchain()
 
-    b.query()
+    #b.query()
+
+    diff = float(b.fetchData("getdifficulty"))
+    print "{:<30s} {:<40g}".format("current diff is", diff)
+    time.sleep(1)
+
+    eta = float(b.fetchData("interval"))
+    print "{:<30s} {:<10f}".format("avg time between blocks (s):", eta)
+    # negative time? wtf?
+    time.sleep(1)
+
+    gHashrate = float(b.fetchData("hashrate"))
+    print "{:<30s} {:<40f}".format("network hashrate (GHs/s):", gHashrate)
+
+    #difficulty=((Time for a block to be found in seconds)*(hashes per second))/2^32
+    diffN = (eta * gHashrate * 10e9) / 2**32
+    print "{:<30s} {:<40g}".format("estimated hashrate:", diffN)
+    print "{:<30s} {:<10f}%".format("estimated increase:",(diffN/diff-1)*100)
+
+    #newDifficulty=currentDifficulty*600/averageTimeBetweenBlocksSinceLastDifficultyChange;
+    #powerInHashesPerSecond=currentDifficulty*2^32/averageTimeBetweenBlocks
+    #averageTimeBetweenBlocks=currentDifficulty*2^32/powerInHashesPerSecond
+
+    # this is just one block, we need the block since the last difficulty change!
+    # and what is 600? ahh ok, this must be the time 
+    diffN2 = diff * 600 / eta
+    print "{:<30s} {:<40g}".format("estimated hashrate2:", diffN2)
+    print "{:<30s} {:<10f}%".format("estimated increase:",(diffN2/diff-1)*100)
+
+    # maybe this:
+    # The network sets the difficulty to the value that would have most likely caused the prior 2016 blocks to take two weeks to complete, given the same computational effort (according to the timestamps recorded in the blocks).
+    timeFor2016 = 2016 * eta
+    weeks = timeFor2016
+    secondsForTwoWeeks = 60*60*24*14
+    print "one week has {:d} s, 2016 blocks need {:f} s".format(secondsForTwoWeeks, timeFor2016)
+    print "{:<30s} {:<10f}%".format("estimated increase:",((secondsForTwoWeeks/timeFor2016)-1)*100)
+
 
     b.printDetails()
 
