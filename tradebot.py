3f99090993f934cbbdf323a6279a72d0461fc333
Markus Meissner <markus@evigo.net>
2015-01-02 23:25:15 +0100

    stupid bot tryouts

--- tradebot.py
+++ tradebot.py
@@ -1,7 +1,8 @@
 import sys, os
 import argparse
 import math
-import time, datetime
+import time, datetime 
+import random
 
 from classes import Havelock
 from classes import Bitcoin
@@ -22,6 +23,7 @@ hl.fetchBalance()
 cash = hl.havelockBalanceAvailable 
 
 hl.portfolio.addSymbol(sym)
+hl.loadTransactionFile(Config.hl_history)
 shares = hl.portfolio.getSymbol(sym).getShareQuantity()
 
 myorders = dict((d["id"], d) for d in orders)
@@ -29,17 +31,20 @@ myorders = dict((d["id"], d) for d in orders)
 print "cash: {} - shares: {} - open orders: {}".format(
         cash, shares, len(myorders))
 
+MIN_AMOUNT = 7
+MAX_AMOUNT = 250
+
 while True:
     myorders = dict((d["id"], d) for d in hl.fetchOrders())
     asks, bids = hl.fetchOrderbook(sym, full=True)
     
     asks = [{"price": float(d["price"]), 
              "amount": float(d["amount"]), 
-             "id": d["id"]} for d in asks.values()]
+             "id": int(d["id"])} for d in asks.values()]
     
     bids = [{"price": float(d["price"]), 
              "amount": float(d["amount"]), 
-             "id": d["id"]} for d in bids.values()]
+             "id": int(d["id"])} for d in bids.values()]
 
     asks.sort(key=lambda o: o["price"])
     bids.sort(reverse=True, key=lambda o: o["price"])
@@ -50,30 +55,37 @@ while True:
     top_ask = asks[0]
     # spread
     spread = top_ask["price"] - top_bid["price"]
+    fee = top_ask["price"] * 0.004
 
+    if spread < fee:
+        print "##### SPREAD TO SMALL!!!!"
+        continue
+
+    myids = [int(x[0]) for x in myorders.items()]
+    
     # check if top in bid:
-    if top_bid["id"] not in myorders:
-        print "##### BID ACTION #####"
-        print "- delete all old orders:"
+    if top_bid["id"] not in myids:
+        amount = random.randint(MIN_AMOUNT, MAX_AMOUNT)
+        print "##### BID ACTION -- delete old -- create new #####"
         for o_id, o in myorders.items():
             if o["type"] == "bid" and o["symbol"] == sym:
-                print "delete: ", hl.cancelOrder(o_id)
-        print "- create new order: "
-        print hl.createOrder(sym, "buy", top_bid["price"]+1e-8, 13)
-        print "##### BID ACTION DONE #####"
+                hl.cancelOrder(o_id)
+        hl.createOrder(sym, "buy", top_bid["price"]+1e-8, amount)
+    else:
+        print "##### NO BID ACTION #####"
 
     
-    if top_ask["id"] not in myorders:
-        print "##### ASK ACTION #####"
-        print "- delete all old orders:"
+    if top_ask["id"] not in myids:
+        amount = random.randint(MIN_AMOUNT, MAX_AMOUNT)
+        print "##### ASK ACTION -- delete old -- create new #####"
         for o_id, o in myorders.items():
             if o["type"] == "ask" and o["symbol"] == sym:
-                print "delete: ", hl.cancelOrder(o_id)
-        print "- create new order: "
-        print hl.createOrder(sym, "sell", top_bid["price"]-1e-8, 13)
-        print "##### ASK ACTION DONE #####"
+                hl.cancelOrder(o_id)
+        hl.createOrder(sym, "sell", top_ask["price"]-1e-8, amount)
+    else:
+        print "##### NO ASK ACTION #####"
 
-    print "top ask: {} bid: {} spread: {}".format(top_ask, top_bid, spread)
+    print "top ask: {} bid: {} spread: {} fee: {}".format(top_ask["price"], top_bid["price"], spread, fee)
     
     time.sleep(4)
 
