794a8120b76500e7a8f233f0d00097e106e6f79c
Markus Meissner <coder@safemailbox.de>
2015-01-09 10:15:26 +0100

    many cleanups - trade

--- tradebot.py
+++ tradebot.py
@@ -15,23 +15,83 @@ from utils import get_console_size
 min_spread = 0.015
 sell_fee = 0.004
 
-def show_balance(havelock_obj, symbol, hours_back):
-     # (timezone -6h) + X hours
-    since = 60*60*6 + 60*60*hours_back
+MIN_MAX_AMOUNT = (200, 550)
+
+min_sleep = 0.1
+max_sleep = 4
+
+min_step = 1e-8
+
+if len(sys.argv) != 2:
+    print "Usage: python2 tradebot.py <symbol>"
+    sys.exit(1)
+
+# take symbol from cmdline
+sym = sys.argv[1]
+
+def get_balance(havelock_obj, symbol, hours_back, timezone=-6):
+     # (timezone correction) + X hours
+    start = time.time() - 60*60*-timezone + 60*60*hours_back
+    end = time.time() - 60*60*-timezone 
 
     p = havelock_obj.portfolio
     t = p.symbols[symbol]
-    havelock_obj.setStartDate(time.time()-(int(since)))
-    havelock_obj.setEndDate(time.time())
-    balance = p.getCurrentWin(symbol)-t.getDividendAmount()
-    print "------> overview ({:>2}h) share balance: {:>8} btc balance: {:>12.8f}". \
-        format(hours_back, t.getShareQuantity(), balance)
 
-    return balance
+    havelock_obj.setStartDate(int(start))
+    havelock_obj.setEndDate(int(end))
+    balance = p.getCurrentWin(symbol) - t.getDividendAmount()
+
+    return t.getShareQuantity(), balance
+
+def show_balance(havelock_obj, symbol, timezone=6):
+    for h in [1, 2, 4, 8, 12, 24, 48]:
+        print "[i] ({:>2}h) shares: {:>8} btc: {:>12.8f}". \
+            format(hours_back, *get_balance(havelock_obj, symbol, h, timezone))
+
+def show_market_info(bids, asks, fee, myids=None):
+    ids = myids or []
+
+    print "{} @ {}".format("-"*60, time.ctime())
+    
+    # market stats:
+    print "[i] spread: {} ({:.3%}) fee: {:.8f}". \
+            format(spread, spread/(asks[0]["price"]), asks[0]["price"]*fee)
+
+    # showing bids / asks
+    for b, a in zip(bids[:10], asks[:10]):
+        print "{:>7d} - {:>12.8f} {}|{:>7d} - {:>12.8f} {}". \
+                format(b["amount"], b["price"], "<-" if b["id"] in ids else "  ",
+                       a["amount"], a["price"], "<-" if a["id"] in ids else "  ")
+
+def clean_orders(otype, symbol, orders):
+    for o_id, o in myorders.items():
+        if o["type"] == otype and o["symbol"] == symbol:
+            hl.cancelOrder(o_id)
+
+def get_bids_asks(symbol):
+    asks, bids = hl.fetchOrderbook(symbol, full=True)
+    
+    asks = [{"price": float(d["price"]), 
+             "amount": int(d["amount"]), 
+             "id": int(d["id"])} for d in asks.values()]
+    
+    bids = [{"price": float(d["price"]), 
+             "amount": int(d["amount"]), 
+             "id": int(d["id"])} for d in bids.values()]
+
+    asks.sort(key=lambda o: o["price"])
+    bids.sort(reverse=True, key=lambda o: o["price"])
+
+    return bids, asks
+
+def update_log(hl_obj):
+    hl_obj.fetchTransactions()
+    hl_obj.fetchPortfolio()
+    hl_obj.fetchBalance()
+    hl_obj.store(Config.hl_history)
+
 
 hl = Havelock(Config)
-
-sym = "PETA"
 
 orders = hl.fetchOrders()
 hl.fetchBalance()
@@ -47,8 +107,6 @@ myorders = dict((d["id"], d) for d in orders)
 print "cash: {} - shares: {} - open orders: {}".format(
         cash, shares, len(myorders))
 
-MIN_MAX_AMOUNT = (200, 550)
-MIN_MAX_SLEEP = (4, 15)
 
 wallet = {"cash": 0.10000, "shares": 100, 
           "sold": 0.00000, "bought": 0.00000,
@@ -60,19 +118,14 @@ current_orders = []
 overview = 10
 
 while True:
-    myorders = dict((d["id"], d) for d in hl.fetchOrders())
-    asks, bids = hl.fetchOrderbook(sym, full=True)
-    
-    asks = [{"price": float(d["price"]), 
-             "amount": float(d["amount"]), 
-             "id": int(d["id"])} for d in asks.values()]
-    
-    bids = [{"price": float(d["price"]), 
-             "amount": float(d["amount"]), 
-             "id": int(d["id"])} for d in bids.values()]
+    time.sleep(random.random() * (max_sleep-min_sleep) + min_sleep)
 
-    asks.sort(key=lambda o: o["price"])
-    bids.sort(reverse=True, key=lambda o: o["price"])
+    # own orders + ids
+    myorders = dict((d["id"], d) for d in hl.fetchOrders())
+    myids = [int(x[0]) for x in myorders.items()]
+    
+    # get current asks/bids
+    bids, asks = get_bids_asks(sym)
 
     # buying shares for price
     top_bid = bids[0]
@@ -82,94 +135,66 @@ while True:
     second_ask = asks[1]
     # spread
     spread = top_ask["price"] - top_bid["price"]
+    # current fee
     fee = top_ask["price"] * sell_fee
+    # best prices 
+    ask_price = top_ask["price"] + min_step 
+    bid_price = top_bid["price"] - min_step
+
+    show_market_info(bids, asks, sell_fee, myids)
     
-    # showing current bid/ask
-    print "-"*60
-    print "SPREAD: {} ({:.3f}%) FEE: {}".format(spread, spread/top_ask["price"]*100, fee)
-    for b, a in zip(bids[:10], asks[:10]):
-        print "{:>7} - {:>12.8f} {}|{:>7} - {:>12.8f} {}". \
-                format(b["amount"], b["price"], "<-" if b["id"] in myids else "  ",
-                       a["amount"], a["price"], "<-" if a["id"] in myids else "  ")
-
-
-    notrade = False
     if spread < fee:
-        print "##### SPREAD TO SMALL - waiting some seconds"
-        time.sleep(random.randint(*MIN_MAX_SLEEP))
-        notrade = True
+        print "[!] SPREAD BELOW FEE {:.8f} - waiting...". \
+            format(fee)
+        continue 
 
-    if spread/top_ask["price"] < min_spread:
-        print "##### SPREAD under 1,5% - wait..."
-        time.sleep(random.randint(*MIN_MAX_SLEEP))
-        notrade = True
-
-    myids = [int(x[0]) for x in myorders.items()]
+    elif spread/top_ask["price"] < min_spread:
+        print "[!] SPREAD BELOW {:.2%} - waiting...". \
+            format(min_spread)
+        continue
  
-    # check range to second 
-    if top_bid["id"] in myids and second_bid["price"] < top_bid["price"]-1e-8:
-        print "-> too much diff in bid - canceling"
+    # check range to second (bid)
+    if top_bid["id"] in myids and second_bid["price"] < top_bid["price"] - min_step:
+        print "-> too much diff ({:.8f}) in bid order - canceling".format(
+            bid_price - second_bid["price"])
         hl.cancelOrder(top_bid["id"])
-    if top_ask["id"] in myids and second_ask["price"] > top_ask["price"]+1e-8:
-        print "-> too much diff in ask - canceling"
+        bid_price = second_bid["price"]
+
+    # check range to second (ask)
+    if top_ask["id"] in myids and second_ask["price"] > top_ask["price"] + min_step:
+        print "-> too much diff ({:.8f}) in ask order - canceling".format(
+            second_ask["price"] - ask_price)
         hl.cancelOrder(top_ask["id"])
+        ask_price = second_ask["price"]
 
-    if not notrade:
-        # check if top in bid:
-        if top_bid["id"] not in myids:
-            amount = random.randint(*MIN_MAX_AMOUNT)
-            print "##### BID ACTION -- delete old -- create new #####"
-            for o_id, o in myorders.items():
-                if o["type"] == "bid" and o["symbol"] == sym:
-                    hl.cancelOrder(o_id)
-            if cash < (top_bid["price"]+1e-8)*amount:
-                print "NOT ENOUGH CASH to buy: {} at {}".format(amount, top_bid["price"])
-            else:
-                hl.createOrder(sym, "buy", top_bid["price"]+1e-8, amount)
-        
-        # check if top in ask:
-        if top_ask["id"] not in myids:
-            amount = random.randint(*MIN_MAX_AMOUNT)
-            print "##### ASK ACTION -- delete old -- create new #####"
-            for o_id, o in myorders.items():
-                if o["type"] == "ask" and o["symbol"] == sym:
-                    hl.cancelOrder(o_id)
-            hl.createOrder(sym, "sell", top_ask["price"]-1e-8, amount)
+    # check if top in bid
+    if top_bid["id"] not in myids:
+        amount = random.randint(*MIN_MAX_AMOUNT)
+        print "[i] BID action, delete existing, create new bid ({} @ {})".\
+                format(amount, bid_price)
+
+        self.clean_orders("buy", sym)
+        hl.createOrder(sym, "buy", bid_price, amount)
     
-        p = hl.portfolio
-    t = p.symbols[sym]
+    # check if top in ask:
+    if top_ask["id"] not in myids:
+        amount = random.randint(*MIN_MAX_AMOUNT)
+        print "[i] ASK action, delete existing, create new ask ({} @ {})". \
+                format(amount, ask_price)
+
+        self.clean_orders("sell", sym)
+        hl.createOrder(sym, "sell", ask_price, amount)
+
     if overview <= 0:
-        overview = 100
-        hl.fetchTransactions()
-        hl.fetchPortfolio()
-        hl.fetchBalance()
-        hl.store(Config.hl_history)
+        overview = 10
+        
+        update_log(hl)
         cash = hl.havelockBalanceAvailable 
+        show_balance(hl, sym)
 
-        overview -= 1
-
-    elif overview % 10 == 0:
-        hl.fetchTransactions()
-        hl.fetchPortfolio()
-        hl.fetchBalance()
-        hl.store(Config.hl_history)
-        cash = hl.havelockBalanceAvailable 
-
-        show_balance(hl, sym, 1)
-        show_balance(hl, sym, 2)
-        show_balance(hl, sym, 4)
-        show_balance(hl, sym, 8)
-        show_balance(hl, sym, 12)
-        show_balance(hl, sym, 24)
-        show_balance(hl, sym, 36)
-        show_balance(hl, sym, 48)
-
-
-        overview -= 1
     else:
         overview -= 1
 
 
-    time.sleep(random.randint(*MIN_MAX_SLEEP))
 
 
